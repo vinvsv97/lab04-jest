f3bd68cbc69a0defed8780cf77e1b62bc614f9cb
iGuest <vinvsv97@uw.edu>
2017-10-27 11:14:44 -0700

    done

--- __tests__/app.spec.js
+++ __tests__/app.spec.js
@@ -4,3 +4,9 @@ import {greet, invertCase} from '../js/app'; //import the functions to test!
 
 /* tests go here */
 
+describe('invertCase() function', function(){
+    it('should convert lowercase to uppercase', function(){
+        let result = invertCase('HellO');
+        expect(result).toEqual('hELLo');
+    });
+})
--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ function greet(name){
  * It may have a few small bugs...
  */
 function invertCase(text) {
-  const LOWER_CASE = "abedefghijklmnoqprstuvwxyz";
+  const LOWER_CASE = "abcdefghijklmnoqprstuvwxyz";
   const UPPER_CASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
   let tempString = '';
@@ -22,11 +22,11 @@ function invertCase(text) {
       tempString += upper;
     }
     else { //is uppercase
-      let lower = LOWER_CASE [ LOWER_CASE.indexOf(char) ];
+      let lower = LOWER_CASE [ UPPER_CASE.indexOf(char) ];
       tempString += lower;
     }
   }
-  return text;
+  return tempString;
 }
 
 
